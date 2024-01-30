48df4f312933b9df8afec95a9d2303e8fd4a2474
Isha-Nanda08 <143190568+Isha-Nanda08@users.noreply.github.com>
2024-01-31 02:22:03 +0530

    add content

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-import express from "express"
-import bodyParser from "body-parser"
-import axios from "axios"
-import pg from "pg"
+import bodyParser from "body-parser";
+import express from "express";
 
 const port = 3000;
 const app = express();
@@ -12,6 +10,13 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
     res.render("index.ejs")
 })
+app.get("/lobby", (req, res) => {
+    res.render("game-room.ejs")
+})
+app.get("/game", (req, res) => {
+    res.render("game-page.ejs")
+})
+
 
 
 app.listen(port, () => { console.log(`Game running at port: ${port}`) })
